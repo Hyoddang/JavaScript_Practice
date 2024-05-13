abdc14fe73898b2092c2f3f0b0a1d8fea930367c
Hyoddang <yhd5689@naver.com>
2024-05-13 17:37:10 +0900

    Todo 추가 버튼 함수 만듦

--- FrontEnd_Animation/Project/TodoList_Project/JS/main.js
+++ FrontEnd_Animation/Project/TodoList_Project/JS/main.js
@@ -1,5 +1,19 @@
 let todoArr = []
 
+let todoValue = document.querySelector(".todo-input");
+
 let todoAddBtn = document.querySelector(".fa-plus").addEventListener('click',function addTodo() {
-  todoArr.push();
+  let contentDiv = document.querySelector('.todo-list');
+
+  todoArr.push(todoValue.value);
+
+  contentDiv.innerHTML += 
+  `<li class="content">
+  <p>${todoValue.value}</p>
+  <div class="content-footer">
+    <button><i class="fa-solid fa-pencil"></i></button>
+    <button><i class="fa-solid fa-xmark"></i></button>
+  </div>
+</li>`
+
 })
