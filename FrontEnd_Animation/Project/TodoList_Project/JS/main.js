e9e2916b0f205e0e8bd0e5b4dd7e4ddbcedd2725
Hyoddang <yhd5689@naver.com>
2024-05-15 16:58:52 +0900

    deleteBtn 재수정

--- FrontEnd_Animation/Project/TodoList_Project/JS/main.js
+++ FrontEnd_Animation/Project/TodoList_Project/JS/main.js
@@ -2,32 +2,47 @@ let todoArr = []
 
 let todoValue = document.querySelector(".todo-input");
 
+let todoIndex = document.querySelectorAll(".content");
+todoIndex.forEach(index => {
+  let todoDataIndex = index.dataset.index;
+})
+
 let todoAddBtn = document.querySelector(".fa-plus").addEventListener('click',function addTodo() {
-  renderTodo()
-  todoValue.value = '';
+  if (todoValue.value) {
+    todoArr.push(todoValue.value);
+    renderTodo()
+    todoValue.value = '';
+  }
 })
 
 function renderTodo() {
   let contentDiv = document.querySelector('.todo-list');
+  contentDiv.innerHTML = ''; // 기존 내용을 지워 중복추가를 방지하는 속성
 
   if(!todoValue.value) {
     return;
   }
 
-  todoArr.push(todoValue.value);
+  todoArr.forEach((todo, index) => {
+    contentDiv.innerHTML += 
+    `<li class="content" data-index="${index}">
+    <p>${todo}</p>
+    <div class="content-footer">
+      <button><i class="fa-solid fa-pencil"></i></button>
+      <button><i class="fa-solid fa-xmark"></i></button>
+    </div>
+  </li>`
+  })
 
-  contentDiv.innerHTML += 
-  `<li class="content">
-  <p>${todoValue.value}</p>
-  <div class="content-footer">
-    <button><i class="fa-solid fa-pencil"></i></button>
-    <button><i class="fa-solid fa-xmark"></i></button>
-  </div>
-</li>`
+  deleteTodo();
 }
 
-let deleteBtn = document.querySelector('.fa-xmark').addEventListener("click", function deleteTodo() {
-  todoArr.filter(todo => {
-    
-  })
-})
+document.querySelector("DOMContentLoaded", function () {
+
+  let deleteBtn = document.querySelector(".fa-xmark");
+  deleteBtn.addEventListener("click", function deleteTodo() {
+    let todoArr = todoArr.filter(i => i !== i);
+
+    renderTodo();
+  });
+});
